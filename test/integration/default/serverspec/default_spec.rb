a61066698be13ae475e3982d2e3d48399dbfafcd
Martin Etmajer <m.etmajer@gmail.com>
2016-03-07 20:34:54 +0100

    Reduce to Dynatrace Server only. Upgrade to Dynatrace 6.3.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -15,41 +15,6 @@ describe file('/opt/dynatrace') do
   it { should be_symlink }
 end
 
-describe file('/opt/dynatrace/agent') do
-  it { should be_directory }
-  it { should be_owned_by 'dynatrace' }
-  it { should be_grouped_into 'dynatrace' }
-end
-
-describe file ('/opt/dynatrace/agent/conf/dtwsagent.ini') do
-  its(:content) { should match /^Name dtwsagent$/ }
-  its(:content) { should match /^Server localhost:9998$/ }
-  its(:content) { should match /^Loglevel info$/ }
-end
-
-describe file('/opt/dynatrace/collector') do
-  it { should be_directory }
-  it { should be_owned_by 'dynatrace' }
-  it { should be_grouped_into 'dynatrace' }
-end
-
-describe file ('/etc/init.d/dynaTraceCollector') do
-  it { should be_owned_by 'root' }
-  it { should be_grouped_into 'root' }
-
-  if os[:family] == 'debian' || os[:family] == 'ubuntu'
-    its(:content) { should match /^\# Default-Start: 2 3 4 5$/ }
-    its(:content) { should match /^\# Default-Stop: 0 1 6$/ }
-  elsif os[:family] == 'redhat'
-    its(:content) { should match /^\# Default-Start: 3 5$/ }
-    its(:content) { should match /^\# Default-Stop: 0 1 2 6$/ }
-  end
-
-  its(:content) { should match /^DT_HOME=\/opt\/dynatrace$/ }
-  its(:content) { should match /^DT_OPTARGS="-listen 9998 -server localhost:6698 -Xms256M -Xmx1024M -XX:PermSize=256m -XX:MaxPermSize=384m"$/ }
-  its(:content) { should match /^.*su - dynatrace -c.*$/ }
-end
-
 describe file ('/etc/init.d/dynaTraceServer') do
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
@@ -83,16 +48,6 @@ describe file ('/etc/init.d/dynaTraceWebServerAgent') do
   its(:content) { should match /^.*su - dynatrace -c.*$/ }
 end
 
-describe process('dtcollector') do
-  it { should be_running }
-  its(:user) { should eq 'dynatrace' }
-  its(:args) { should match /-listen 9998/ }
-  its(:args) { should match /-Xms256M/ }
-  its(:args) { should match /-Xmx1024M/ }
-  its(:args) { should match /-XX:PermSize=256m/ }
-  its(:args) { should match /-XX:MaxPermSize=384m/ }
-end
-
 describe process('dtfrontendserver') do
   it { should be_running }
   its(:user) { should eq 'dynatrace' }
@@ -104,32 +59,7 @@ describe process('dtserver') do
   its(:args) { should match /-listen 6698/ }
 end
 
-describe process('dtwsagent') do
-  it { should be_running }
-  its(:user) { should eq 'dynatrace' }
-end
-
-describe service('dynaTraceCollector') do
-  it { should be_enabled }
-
-  if os[:family] == 'debian' || os[:family] == 'ubuntu'
-      it { should be_enabled.with_level(3) }
-      it { should be_enabled.with_level(4) }
-      it { should be_enabled.with_level(5) }
-  end
-end
-
 describe service('dynaTraceServer') do
-  it { should be_enabled }
-
-  if os[:family] == 'debian' || os[:family] == 'ubuntu'
-      it { should be_enabled.with_level(3) }
-      it { should be_enabled.with_level(4) }
-      it { should be_enabled.with_level(5) }
-  end
-end
-
-describe service('dynaTraceWebServerAgent') do
   it { should be_enabled }
 
   if os[:family] == 'debian' || os[:family] == 'ubuntu'
@@ -156,10 +86,6 @@ describe port(8021) do
 end
 
 describe port(9911) do
-  it { should be_listening }
-end
-
-describe port(9998) do
   it { should be_listening }
 end
 
